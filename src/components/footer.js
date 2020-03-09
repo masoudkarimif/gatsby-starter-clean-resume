b6bb39f9b79a7f357a24c6f2c3c76ef53377b778
masoudkarimif <karimif.masoud@gmail.com>
2020-03-09 12:49:09 -0600

    Changed footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = (props)=> {
         <div className="row">
             <div className="column">
                <p id="cp">&copy; {props.name}</p>
-               <p id="by-gatsby">Proudly Made with <a target="_blank" rel="noreferrer noopener" href="https://gatsbyjs.com">Gatsby</a></p>
+               <p id="by-gatsby">Proudly made with <a target="_blank" rel="noreferrer noopener" href="https://gatsbyjs.com">Gatsby</a></p>
             </div>
         </div>
     </div>
